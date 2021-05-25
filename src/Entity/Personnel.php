8f551d770cdb8b6d2930b80206b6ab883b0bf6f7
TurgutSubay <m.turguts@hotmail.com>
2021-05-26 00:17:40 +0300

    navbar fixed

---
+++ src/Entity/Personnel.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonnelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonnelRepository::class)
+ */
+class Personnel
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $position;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $office;
+
+    /**
+     * @ORM\Column(type="datetime",options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $created_date;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true, columnDefinition="DATETIME on update CURRENT_TIMESTAMP")
+     */
+    private $updated_date;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPosition(): ?string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getOffice(): ?int
+    {
+        return $this->office;
+    }
+
+    public function setOffice(string $office): self
+    {
+        $this->office = $office;
+
+        return $this;
+    }
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $created_date): self
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updated_date;
+    }
+
+    public function setUpdatedDate(?\DateTimeInterface $updated_date): self
+    {
+        $this->updated_date = $updated_date;
+
+        return $this;
+    }
+
+}
