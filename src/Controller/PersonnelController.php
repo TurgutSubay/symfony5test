f07716edef912eb2bf53513ccb80ab46b7d1fc61
TurgutSubay <m.turguts@hotmail.com>
2021-05-29 18:32:49 +0300

    Datatable

--- src/Controller/PersonnelController.php
+++ src/Controller/PersonnelController.php
@@ -8,7 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
+use Doctrine\ORM\EntityManager;
+use App\Repository\PersonnelRepository;
 
 class PersonnelController extends AbstractController
 {
@@ -18,26 +19,23 @@ class PersonnelController extends AbstractController
     public function index(): Response
     {
         return $this->render('personnel/index.html.twig', []);
-
     }
 
     /**
      * @Route("/personnelData", name="personnelData", methods={"GET","POST"})
      * @param $request
      */
-    public function dataTableRead(Request $request)
+    public function dataTableRead(Request $request, PersonnelRepository $personnelRepository)
     {
-        $draw = $request->get('draw');
-        $repository = $this->getDoctrine()->getRepository(Personnel::class);
-        $products = $repository->findAll();
         $result = true;
         $recordsTotal = 0;
-        foreach ($products as $item) {
+        $personnel = $personnelRepository->JoinOffice();
+        foreach ($personnel as $item) {
             $data[] = [
-                'id' => $item->getId(),
-                'name' => $item->getName(),
-                'office' => $item->getOffice(),
-                'position' => $item->getPosition(),
+                'id' => $item[0]->getId(),
+                'name' => $item[0]->getName(),
+                'office' => $item['officeName'],
+                'position' => $item[0]->getPosition(),
             ];
         }
         if (!isset($data)) {
@@ -47,6 +45,7 @@ class PersonnelController extends AbstractController
             $recordsTotal = count($data);
         }
 
+        $draw = $request->get('draw');
         return $this->json([
             'draw' => $draw,
             'result' => $result,
